7c1c7bd69d60e3e725ca4eee78bffa47340d3064
Cristiano Lopes Duarte <tkdcris@gmail.com>
2023-07-15 00:19:13 -0300

    Domain model

--- src/main/java/com/devsuperior/dslist/controllers/GameController.java
+++ src/main/java/com/devsuperior/dslist/controllers/GameController.java
@@ -1,7 +1,6 @@
 package com.devsuperior.dslist.controllers;
 
 import com.devsuperior.dslist.dto.GameMinDTO;
-import com.devsuperior.dslist.entities.Game;
 import com.devsuperior.dslist.services.GameService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,7 +18,6 @@ public class GameController {
 
     @GetMapping
     public List<GameMinDTO> findAll() {
-        List<GameMinDTO> result = gameService.findAll();
-        return result;
+        return gameService.findAll();
     }
 }
